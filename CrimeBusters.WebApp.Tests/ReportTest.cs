55d7638715777d353b540cbc1121cd140325bf51
gitterchris <ababan2@illinois.edu>
2014-03-20 15:23:04 +0400

    Committing the Test Project

---
+++ CrimeBusters.WebApp.Tests/ReportTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrimeBusters.WebApp.Models.Report;
+using CrimeBusters.WebApp.Models.Users;
+using CrimeBusters.WebApp.Models.DAL;
+using System.Collections.Generic;
+
+namespace CrimeBusters.WebApp.Tests
+{
+    [TestClass]
+    public class ReportTest
+    {
+        private Report _testReport;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _testReport = new Report(
+               ReportTypeEnum.ALERT,
+               "Test message",
+               "40.104669",
+               "-88.242254",
+               "",
+               DateTime.UtcNow,
+               new User("test.user"));
+            _testReport.CreateReport();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _testReport = null;
+            ReportsDAO.DeleteReportTest();
+        }
+
+        [TestMethod]
+        public void TestCreateReport()
+        {
+            Report report = new Report(
+                ReportTypeEnum.ALERT, 
+                "Test message", 
+                "40.104669", 
+                "-88.242254", 
+                "", 
+                DateTime.UtcNow, 
+                new User("test.user"));
+            string result = report.CreateReport();
+       
+            Assert.IsTrue(result.Equals("success"), "The method should return success.");
+        }
+
+        [TestMethod]
+        public void TestGetReports()
+        {
+            List<Report> reports = Report.GetReports();
+            Assert.IsTrue(reports.Count > 0);
+        }
+    }
+}
---
+++ CrimeBusters.WebApp.Tests/UserTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrimeBusters.WebApp.Models.Users;
+
+namespace CrimeBusters.WebApp.Tests
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void TestGetUserIsNotNull()
+        {
+            User testUser = User.GetUser("test.user");
+            Assert.IsNotNull(testUser);
+        }
+
+        [TestMethod]
+        public void TestGetUserDetails()
+        {
+            User testUser = User.GetUser("test.user");
+            Assert.IsTrue(testUser.UserName.Equals("test.user"));
+            Assert.IsTrue(testUser.FirstName.Equals("Test"), "First name should be Test.");
+            Assert.IsTrue(testUser.LastName.Equals("User"), "Last name should be User.");
+            Assert.IsTrue(testUser.Gender.Equals("M"), "Gender should be M.");
+            Assert.IsTrue(testUser.Email.Equals("test@test.com"), "Email should be test@test.com.");
+            Assert.IsTrue(testUser.PhoneNumber.Equals("+1234567890"), "PhoneNumber should be +1234567890.");
+            Assert.IsTrue(testUser.Address.Equals("Urbana"), "Address should be Urbana.");
+            Assert.IsTrue(testUser.ZipCode.Equals("51423"), "Zipcode should be 51423.");
+        }
+    }
+}
